4e62b2f502e7a11c0add142c91b9b10914553654
jfmam <jfmam@naver.com>
2022-02-18 12:15:50 +0900

    add husk and set git hooks

--- packages/yosm-front/next.config.js
+++ packages/yosm-front/next.config.js
@@ -1,10 +1,12 @@
- /** @type {import('next').NextConfig} */
-
+/** @type {import('next').NextConfig} */
 const withTM = require("next-transpile-modules")(["shared"]);
 const withPlugins = require("next-compose-plugins");
 
 const nextConfig = {
   reactStrictMode: true,
+    eslint: {
+    dirs: ['pages'],
+  },
 }
 
 module.exports = nextConfig;

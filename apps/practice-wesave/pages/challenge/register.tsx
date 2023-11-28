d1e4fba819b4370ad48bc05857e7becc64ffeb42
jfmam <jfmam@naver.com>
2023-11-28 19:48:24 +0900

    feat: add calender, divide component

--- apps/practice-wesave/pages/challenge/register.tsx
+++ apps/practice-wesave/pages/challenge/register.tsx
@@ -1,46 +1,35 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement, useCallback, useEffect, useState } from 'react';
 import cn from 'classnames/bind';
-import * as yup from 'yup';
-import { useFormik } from 'formik';
+import { toast, Toaster } from 'react-hot-toast';
+import ChallengeCreateForm from '@/components/section/Challenge/Form';
 
 import { ChallengeLayout, NotificationLayout } from '@/components/template';
-import { ChallengeCreateForm, InitialValuesType } from '@/components/section';
 import { useRegistChallenges } from '@/hooks/quries/challenge/useRegistChallenges';
 import styles from '@/styles/Register.module.scss';
 import { PrimaryBtn } from '@/components/atom';
+import { Challenge } from '@/common';
 
 const cx = cn.bind(styles);
-
-const registerSchema = yup.object().shape({
-  name: yup.string().max(20).required('챌린지 이름을 작성해 주세요.'),
-  goal: yup.string().max(20).required('목표를 설정해 주세요.'),
-  actionDay: yup.array().min(1, '최소 1개이상 선택하야 합니다.'),
-  badge: yup.string().required('뱃지를 선택해 주세요'),
-});
 
 export default function Register() {
   const [isSuccess, setIsSuccess] = useState(false);
   const { mutate, isLoading, isError } = useRegistChallenges();
-  const formik = useFormik<InitialValuesType>({
-    initialValues: {
-      name: '',
-      type: 'save',
-      actionDay: [],
-      endDate: '',
-      goal: '',
-      startDate: '',
-      badge: 'pig',
-    },
-    onSubmit: (challenge: InitialValuesType, { resetForm }) => {
+  const onSubmit = useCallback(
+    (challenge: Challenge) => {
       mutate(challenge, {
         onSuccess: () => {
-          resetForm();
           setIsSuccess(true);
         },
       });
     },
-    validationSchema: registerSchema,
-  });
+    [mutate],
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error('챌린지 생성에 실패하였습니다.');
+    }
+  }, [isError]);
 
   if (isSuccess) {
     return (
@@ -59,7 +48,12 @@ export default function Register() {
     );
   }
 
-  return <ChallengeCreateForm isError={isError} isLoading={isLoading} formik={formik} />;
+  return (
+    <>
+      <ChallengeCreateForm callback={onSubmit} isError={isError} isLoading={isLoading} />
+      {isError && <Toaster position="top-center" />}
+    </>
+  );
 }
 
 Register.getLayout = function getLayout(page: ReactElement) {

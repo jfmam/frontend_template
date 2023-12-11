cae0831d099f18c67cd99c982ea2caa34dafd297
jfmam <jfmam@naver.com>
2023-12-11 23:16:37 +0900

    feat: connect my achievement api

--- apps/practice-wesave/hooks/quries/challenge/useFetchMyAchievement.ts
+++ apps/practice-wesave/hooks/quries/challenge/useFetchMyAchievement.ts
@@ -5,17 +5,20 @@ import { PaginationResponse, AchivementResponse } from '@/common';
 
 const pageSize = 15;
 
-export const getMyAchivements = async (token: string, offset = 1): Promise<PaginationResponse<AchivementResponse>> => {
+export const getMyAchivements = async (
+  token: string,
+  options?: { lastKey?: string; offset?: number },
+): Promise<PaginationResponse<AchivementResponse>> => {
   const myAchievements = await new AchievementAPI(token).getMyAchievements({
     limit: pageSize,
-    offset: offset,
+    offset: options?.offset || 1,
+    lastKey: options?.lastKey,
   });
 
   const result: PaginationResponse<AchivementResponse> = {
     items: myAchievements.items,
-    isLastPage: myAchievements.isLastPage,
     limit: pageSize,
-    offset,
+    offset: options?.offset || 1,
     lastKey: myAchievements.lastKey,
   };
 
@@ -25,11 +28,15 @@ export const getMyAchivements = async (token: string, offset = 1): Promise<Pagin
 export function useFetchMyAchievements(token: string) {
   return useInfiniteQuery<PaginationResponse<AchivementResponse>, AxiosError, PaginationResponse<AchivementResponse>>(
     'my-achievements',
-    ({ pageParam }) => getMyAchivements(token, pageParam?.offset),
+    ({ pageParam }) =>
+      getMyAchivements(token, {
+        offset: pageParam?.offset,
+        lastKey: pageParam?.lastKey,
+      }),
     {
       getNextPageParam: lastPage => {
-        if (lastPage.isLastPage) return undefined;
-        return { offset: lastPage.offset + 1 };
+        if (!lastPage.lastKey) return undefined;
+        return { offset: lastPage.offset + 1, lastKey: lastPage.lastKey };
       },
       useErrorBoundary: true,
       suspense: true,

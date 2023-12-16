85ad8ae8196247c2a6bed1b01e606414175ffb2a
jfmam <jfmam@naver.com>
2023-12-16 22:33:26 +0900

    build: remove unused file

--- apps/practice-wesave/pages/api/challenge/[id].ts
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { ChallengeResponse } from '@/common';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<ChallengeResponse[]>) {
-  if (req.method === 'POST') return res.status(201);
-}
